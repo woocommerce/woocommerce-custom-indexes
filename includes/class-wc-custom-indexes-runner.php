bfc2dc155b26121063fa15be91a4ddac1e4f81be
Rodrigo Primo <rodrigosprimo@gmail.com>
2018-06-07 14:05:41 -0300

    Put the site in maintenance mode before adding indexes
    Using the same method used by WP to put the site in maintenance mode while doing an update. Simply creates a .maintenance file and let WP do the rest. The file will be removed after the database queries finish execution. If the script is aborted before the .maintenance file is removed, WordPress will remove it after 10 minutes.

--- includes/class-wc-custom-indexes-runner.php
+++ includes/class-wc-custom-indexes-runner.php
@@ -18,11 +18,28 @@ class WC_Custom_Indexes_Runner {
 	 * @return void
 	 */
 	public function add_indexes() {
-		global $wpdb;
+		global $wpdb, $wp_filesystem;
+
+		// Attempt to initialize WP_Filesystem class.
+		if ( ! WP_Filesystem() ) {
+			wp_die( esc_html__( 'Unable to put site in maintenance mode. Aborting.', 'woocommerce-custom-indexes' ) );
+		}
+
+		// Create maintenance file to put WP in maintenance mode while running the queries. Copied from wp-admin/includes/update-core.php.
+		$maintenance_string = '<?php $upgrading = ' . time() . '; ?>';
+		$maintenance_file = ABSPATH . '.maintenance';
+		$wp_filesystem->delete( $maintenance_file );
+		$wp_filesystem->put_contents( $maintenance_file, $maintenance_string, FS_CHMOD_FILE );
 
 		// phpcs:ignore WordPress.VIP.DirectDatabaseQuery.NoCaching
 		$wpdb->query(
 			"ALTER TABLE {$wpdb->postmeta} ADD INDEX wc_meta_key_value (meta_key(191), meta_value(100))" // phpcs:ignore WordPress.VIP.DirectDatabaseQuery.SchemaChange
 		);
+
+		// Remove maintenance file.
+		$wp_filesystem->delete( $maintenance_file );
+
+		wp_safe_redirect( admin_url( 'admin.php?page=wc-custom-indexes' ) );
+		exit;
 	}
 }
